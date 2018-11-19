642632fb04187ad28d88ef091222b925b5839c9d
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-19 12:05:01 +0100

    exo8

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int = 200;
+    while ($int >0) {
+      echo 'Enfin' . '<br>';
+      $int -= 12;
+    }
+    ?>
+  </body>
